8422cf159d20b7ecf8a58cc2516cbf934c6f1002
Rohan <redkarrohan2002@gmail.com>
2023-06-13 19:47:08 +0530

    Introduced a player to the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+
+import {useState} from 'react';
 import './components/styles.scss';
 import Board from './components/Board';
 
+
 function App() {
+
+  
+
  return (
+
       <div className ="app">
+        <div>
+          <button onClick={onBtnClick}> Click here! </button>
+          
+        </div>
         <Board />
       </div>
  );
 }
 
-export default App; 
+export default App;  
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,7 +2,7 @@
 
 const Square = ({value}) => {
     return (
-        <button type ="button" className="square">
+        <button type ="button" className="square" onClick={onBtnClick}>
             {value}
         </button>
     )
