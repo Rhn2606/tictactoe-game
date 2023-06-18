3da7771fb44522dfc499f5cf40a18a42327d1e8a
Rohan <redkarrohan2002@gmail.com>
2023-06-18 12:26:44 +0530

    display draw message, applying styles

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,59 +1,37 @@
-import {useState} from 'react';
-import Square from "./Square";
+import Square from './Square';
 
-const Board = () => {
-
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [isXNext, setIsXNext] = useState(false);
-
-    const handleSquareClick = (clickedPosition) => {
-        if(squares[clickedPosition]) {
-            return;
-        }
-
-        setSquares((currentSquares) => {
-            return currentSquares.map((squareValue, position) => {
-                if (clickedPosition === position) {
-                    return isXNext ? 'X' : 'O';
-                }
-
-                return squareValue;
-            });
-        });
-
-        setIsXNext(currentIsXNext => !currentIsXNext);
-
-    };
-    
-    const renderSquare = position => {
-        return (
-            <Square
-            value={squares[position]}
-            onClick={() => handleSquareClick(position)}
-            />
-        )
-    }
+const Board = ({ squares, handleSquareClick, winningSquares }) => {
+  const renderSquare = position => {
+    const isWinningSquare = winningSquares.includes(position);
 
     return (
-        <div className="board">
-            <div className="board-row">
-                {renderSquare(0)}
-                {renderSquare(1)}
-                {renderSquare(2)}
-            </div>
-            <div className="board-row">
-                {renderSquare(3)}
-                {renderSquare(4)}
-                {renderSquare(5)}
-                
-            </div>
-            <div className="board-row">
-                {renderSquare(6)}
-                {renderSquare(7)}
-                {renderSquare(8)}
-            </div>
-        </div>
+      <Square
+        value={squares[position]}
+        onClick={() => handleSquareClick(position)}
+        isWinningSquare={isWinningSquare}
+      />
     );
+  };
+
+  return (
+    <div className="board">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 };
 
 export default Board;
